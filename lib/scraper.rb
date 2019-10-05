35b01a6e3e1112e2ee1dffed9192ae342c6ae45f
Baktiar Miah <baktiar.m@gmail.com>
2019-10-05 22:17:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("https://www.priceline.com")
 doc = Nokogiri::HTML(html)
-courses =  doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
-
+#courses =  doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+puts doc 
 # courses.each do |course|
 #   puts course.text.strip
 # end 
